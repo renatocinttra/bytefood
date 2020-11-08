8e80676b8a32d1816fc25e775ab81cfc63dd0a8f
renatocinttra <renatocinttra@gmail.com>
2020-11-08 08:12:56 -0300

    Add fisrt Blueprint

---
+++ bytefood/ext/site/main.py
@@ -0,0 +1,12 @@
+from flask import request, render_template
+from flask import Blueprint
+
+bp = Blueprint("site", __name__)
+
+
+@bp.route("/")
+def index():
+    return render_template(
+        "index.html",
+        name=request.args['name']
+    )
